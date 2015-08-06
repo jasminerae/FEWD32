b21533ddb737b7036db793d7bd4b021402215662
jasminerae <jasmine@Caecilia.local>
2015-08-05 20:40:14 -0700

    More son stuff

--- Final/cards.js
+++ Final/cards.js
@@ -6,6 +6,20 @@ function fetchData() {
   $.get('http://steelringers.firebaseio.com/kizze.json', function(spells) {
     for (var i = 0; i < spells.length; i++) {
       console.log(spells[i]);
+
+      if (Array.isArray(spells[i].components)) {
+        spells[i].components = spells[i].components.join(', ');
+      }
+
+      if (spells[i].savingThrow == "None"){
+        spells[i].savingThrow = null
+      }
+
+      if (!spells[i].spellResistance){
+        spells[i].spellResistance = "No Spell Resistance"
+      }
+      else {spells[i].spellResistance = null}
+
       var renderedTemplate = Mustache.render(rawTemplate, spells[i]);
       $('#spellBook').append(renderedTemplate);
     };
